b8ecc1e797208a9c6c16f67b70eee65bc125b5af
Alex Nico <a.nicolaidis@outlook.com>
2023-02-04 09:42:23 +1000

    Added unit tests for the classes

--- name_sorter.py
+++ name_sorter.py
@@ -1,46 +1,28 @@
 import sys
 
 class TextFileReader:
-  """ 
-  This class is for reading a text file producing a list of lines in the file
-  """
   def __init__(self, file_name):
     self.file_name = file_name
 
   def list_of_lines(self):
     with open(self.file_name, mode = 'r') as file:
       lines = file.read()
+
       lines_list = lines.splitlines()
       return lines_list
 
 
 class TextFileWriter: 
-  ""
   def __init__(self, list):
     self.list = list
 
   def generate_text_file(self, new_file_name):
     with open(new_file_name, mode='w',newline='') as file:
-      for line in self.list:
-        file.write(line + '\n') # TODO remove the trailing new line character in the text file
-
-#Thinking about combining into 1 text file handler below ....
-
-# class TextFileHandler:
-#     def __init__(self, file_name):
-#         self.file_name = file_name
-
-#     def list_of_lines(self):
-#         with open(self.file_name, mode='r') as file:
-#             lines = file.read()
-#             lines_list = lines.splitlines()
-#             return lines_list
-
-#     def generate_text_file(self, new_file_name, lines_list):
-#         with open(new_file_name, mode='w', newline="") as file:
-#             for line in lines_list:
-#                 file.write(line + '\n')
-
+      for i, line in enumerate(self.list):
+        if(i != len(self.list)-1):
+          file.write(line + '\n')
+        else:
+          file.write(line)
 
 
 class NameSorter:
@@ -57,16 +39,16 @@ class NameSorter:
 
     # break each name into a list of given names and last name
   
-    for i in self.list_of_names:
-      split_names = i.split(' ')
+    for full_name in self.list_of_names:
+      split_names = full_name.split(' ')
       #reorder with last name at the front
       person_name_last_first = split_names.pop() + " " + " ".join(split_names)
       last_names_first_list.append(person_name_last_first)
     
     last_names_first_list.sort()
 
-    for i in last_names_first_list:
-      split_names = i.split(' ')
+    for full_name_last_first in last_names_first_list:
+      split_names = full_name_last_first.split(' ')
       #reconstruct name moving last name to the end
       last_name = split_names.pop(0)
       person_full_name = " ".join(split_names) + " " + last_name
@@ -76,10 +58,17 @@ class NameSorter:
   
 
 if __name__ == '__main__':
-  file_path = sys.argv[1] # allows user to pass an additional argument in the command line (list to sort)
-  names_list = TextFileReader(file_path).list_of_lines()
-  sorted_names = NameSorter(names_list).sort_by_last_name()
-  new_file = TextFileWriter(sorted_names).generate_text_file('sorted-names.txt')
+  try:
+    file_path = sys.argv[1] # allows user to pass an additional argument in the command line (list to sort)
+    try:
+      names_list = TextFileReader(file_path).list_of_lines()
+      sorted_names = NameSorter(names_list).sort_by_last_name()
+      print(*sorted_names, sep='\n')
+      new_file = TextFileWriter(sorted_names).generate_text_file('sorted-names.txt')
+    except:
+      print('Not a valid file please pass a .txt as the command line argument')
+  except:
+    print('Text file of names to be passed as a command line argument')
 
 
 
---
+++ test_name-sorter.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+from name_sorter import NameSorter as ns
+from name_sorter import TextFileReader as tfr
+from name_sorter import TextFileWriter as tfw
+
+class TestNameSorter(unittest.TestCase):
+  def test_sort_by_last_name(self):
+    sorted_name_list = ns(['Alex Nico', 'Andrea Lawton', 'Peter Michael Kid']).sort_by_last_name()
+    self.assertEqual(sorted_name_list, ['Peter Michael Kid','Andrea Lawton','Alex Nico'])
+
+
+class TestTextFileReader(unittest.TestCase):
+  def setUp(self):
+    with open('test.txt', mode='w', newline='') as file:
+        file.write("file line1\nfile line2\nfile line3")
+    
+  def test_list_of_lines(self):
+    list_of_file_lines = tfr('test.txt').list_of_lines()
+    self.assertEqual(list_of_file_lines, ['file line1','file line2','file line3'])
+
+  def tearDown(self):
+    os.remove('test.txt')
+
+
+class TestTextFileWriter(unittest.TestCase):
+  def setUp(self):
+    string_list = ['file line1','file line2','file line3']
+    tfw(string_list).generate_text_file('test.txt')
+    
+  def test_generate_text_file(self):
+    with open('test.txt', mode='r', newline='') as file:
+      file_string = file.read()
+      self.assertEqual(file_string, "file line1\nfile line2\nfile line3")
+    
+  def tearDown(self):
+    os.remove('test.txt')
+  
+
+    
+
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+
+
